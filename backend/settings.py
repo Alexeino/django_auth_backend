f9e29f197785f09a6a4825057e5534e16ab94119
Alexeino <ms8750122327@gmail.com>
2022-05-12 17:28:12 +0530

    Protected Test View Added

--- backend/settings.py
+++ backend/settings.py
@@ -37,14 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'accounts',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'jwt_backend'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -132,7 +135,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
     'ROTATE_REFRESH_TOKENS': True,   # To get a new refresh token everytime we refresh or obtain tokens
     'BLACKLIST_AFTER_ROTATION': True,
@@ -145,3 +148,5 @@ SIMPLE_JWT = {
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
     'TOKEN_TYPE_CLAIM': 'token_type',
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
--- jwt_backend/urls.py
+++ jwt_backend/urls.py
@@ -1,8 +1,9 @@
 from .views import CustomTokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
 from django.urls import path
-
+from accounts.views  import  TestView
 urlpatterns = [
     path('token/',CustomTokenObtainPairView.as_view(),name='get-token'),
-    path('token/refresh/',TokenRefreshView.as_view(),name='refresh-token')
+    path('token/refresh/',TokenRefreshView.as_view(),name='refresh-token'),
+    path('test/',TestView.as_view(),name="test")
 ]
